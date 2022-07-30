f0249ac2661c973e177e0734ee2514c2840cc3bd
Khizar Panchoo <khizarp@gmail.com>
2022-07-30 19:23:39 +0400

    smf

--- shoppingcart/cart.php
+++ shoppingcart/cart.php
@@ -4,24 +4,23 @@ if (isset($_POST['product_id'], $_POST['quantity']) && is_numeric($_POST['produc
     // Set the post variables so we easily identify them, also make sure they are integer
     $product_id = (int)$_POST['product_id'];
     $quantity = (int)$_POST['quantity'];
-    // Prepare the SQL statement, we basically are checking if the product exists in our databaser
+    // Prepare the SQL statement
     $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
     $stmt->execute([$_POST['product_id']]);
-    // Fetch the product from the database and return the result as an Array
+    //
     $product = $stmt->fetch(PDO::FETCH_ASSOC);
-    // Check if the product exists (array is not empty)
+    // Check if the product exists 
     if ($product && $quantity > 0) {
-        // Product exists in database, now we can create/update the session variable for the cart
+        // 
         if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
             if (array_key_exists($product_id, $_SESSION['cart'])) {
-                // Product exists in cart so just update the quanity
+                //
                 $_SESSION['cart'][$product_id] += $quantity;
             } else {
-                // Product is not in cart so add it
+                // 
                 $_SESSION['cart'][$product_id] = $quantity;
             }
         } else {
-            // There are no products in cart, this will add the first product to cart
             $_SESSION['cart'] = array($product_id => $quantity);
         }
     }
@@ -29,21 +28,21 @@ if (isset($_POST['product_id'], $_POST['quantity']) && is_numeric($_POST['produc
     header('location: index.php?page=cart');
     exit;
 }
-// Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart
+// Remove product from cart
 if (isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['remove']])) {
-    // Remove the product from the shopping cart
+    // Remove the product 
     unset($_SESSION['cart'][$_GET['remove']]);
 }
-// Update product quantities in cart if the user clicks the "Update" button on the shopping cart page
+// Update product quantities 
 if (isset($_POST['update']) && isset($_SESSION['cart'])) {
-    // Loop through the post data so we can update the quantities for every product in cart
+    //
     foreach ($_POST as $k => $v) {
         if (strpos($k, 'quantity') !== false && is_numeric($v)) {
             $id = str_replace('quantity-', '', $k);
             $quantity = (int)$v;
-            // Always do checks and validation
+            // 
             if (is_numeric($id) && isset($_SESSION['cart'][$id]) && $quantity > 0) {
-                // Update new quantity
+                //
                 $_SESSION['cart'][$id] = $quantity;
             }
         }
--- shoppingcart/functions.php
+++ shoppingcart/functions.php
@@ -1,7 +1,7 @@
 <?php
 function pdo_connect_mysql()
 {
-    // Update the details below with your MySQL details
+    // 
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
     $DATABASE_PASS = '';
@@ -9,11 +9,11 @@ function pdo_connect_mysql()
     try {
         return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
-        // If there is an error with the connection, stop the script and display the error.
+
         exit('Failed to connect to database!');
     }
 }
-// Template header, feel free to customize this
+
 function template_header($title)
 {
     echo <<<EOT
@@ -44,7 +44,7 @@ function template_header($title)
         <main>
 EOT;
 }
-// Template footer
+
 function template_footer()
 {
     $year = date('Y');
@@ -59,5 +59,5 @@ function template_footer()
 </html>
 EOT;
 }
-// Get the amount of items in the shopping cart, this will be displayed in the header.
+
 $num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
